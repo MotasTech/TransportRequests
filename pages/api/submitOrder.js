c0bc78b99139b6867ef96cba9613d2f6c33ff881
MotasTech <151187784+MotasTech@users.noreply.github.com>
2023-11-23 09:34:09 -0500

    Add files via upload

---
+++ pages/api/submitOrder.js
@@ -0,0 +1,27 @@
+import axios from 'axios'; // Import Axios
+
+// pages/api/submitOrder.js
+const GOOGLE_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxlkj0yVujdqfj88oYa_4tHnJRvtGP-HxiuRYxvFNQjhZ8W_fFYbluJba9zUhPLJ35i/exec';
+
+
+export default function handler(req, res) {
+  console.log('here')
+  if (req.method === 'POST') {
+    const { passcode, company, notes, orders } = req.body;
+
+
+    axios.post(GOOGLE_ENDPOINT, { passcode, company, notes, orders })
+    .then((response) => {
+      res.status(response.status).json(response.data);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+
+    // For this example, we're just sending a success response
+    res.status(200).json({ success: true });
+  } else {
+    // Return an error for other HTTP methods
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,105 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import EmployeeTable from '../components/EmployeeTable';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="bg-dark mb-0">
+        <main className="mb-0 pt-0 pb-2">
+          <img src="/Motas-logo-primary-white.png" className="w-25 py-2 mt-5 mb-3" />
+          <h2 className="text-white">TRANSPORT REQUESTS</h2>
+        </main>
+      </div>
+
+      <main className="mt-1 pt-2 pb-4">
+        {/* Rest of your content */}
+        <EmployeeTable />
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Terms go here
+        </a>
+      </footer>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family:
+            Menlo,
+            Monaco,
+            Lucida Console,
+            Liberation Mono,
+            DejaVu Sans Mono,
+            Bitstream Vera Sans Mono,
+            Courier New,
+            monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
