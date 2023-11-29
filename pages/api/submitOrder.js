13f5f7d8630145bb54b509cca640ceefcbdaf0e7
MotasTech <151187784+MotasTech@users.noreply.github.com>
2023-11-28 21:42:16 -0500

    Fixed email issue

--- pages/api/submitOrder.js
+++ pages/api/submitOrder.js
@@ -1,7 +1,7 @@
 import axios from 'axios'; // Import Axios
 
 // pages/api/submitOrder.js
-const GOOGLE_ENDPOINT = 'https://script.google.com/macros/s/AKfycby6h7JvK09kph_-NOdrca6AFrMfr2hJRxdC3-Tt8C87k5UK4vRqCqvX3Mt7GbiVzlbn/exec?email=yes';
+const GOOGLE_ENDPOINT = 'https://script.google.com/macros/s/AKfycby6h7JvK09kph_-NOdrca6AFrMfr2hJRxdC3-Tt8C87k5UK4vRqCqvX3Mt7GbiVzlbn/exec?email=Yes';
 
 
 
@@ -13,7 +13,7 @@ export default function handler(req, res) {
 
     axios.post(GOOGLE_ENDPOINT, { passcode, company, notes, orders })
     .then((response) => {
-      console.log('Google Response',response);
+      console.log('Google Response',response.data);
       res.status(response.status).json(response.data);
     })
     .catch((error) => {
