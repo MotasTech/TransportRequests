616a53d12c7b2367121a47fd54d4189c733be805
MotasTech <151187784+MotasTech@users.noreply.github.com>
2023-12-13 15:08:08 -0500

    updated title

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Motas Requests</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
