156ec3719a99e0e42d1399ec9bce1d15383f1b42
MotasTech <151187784+MotasTech@users.noreply.github.com>
2024-10-08 18:04:31 -0400

    Add files via upload

--- pages/api/logRFID.js
+++ pages/api/logRFID.js
@@ -5,11 +5,36 @@ const GOOGLE_RFID_ENDPOINT = process.env.NEXT_PUBLIC_GOOGLE_RFID_ENDPOINT;
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
+    var requestBody = req.body;
 
     // Get the query parameter `?reader=` from the URL
     const { reader } = req.query;
 
     // If the reader parameter exists, add it to the payload
+    if (Array.isArray(req.body)) {
+      // data
+      var requestBody = {};
+      req.body.forEach(obj => {
+        requestBody[obj.data.idHex] = {
+          'timestamp': obj.timestamp,
+          'reader': reader
+        }
+      })
+    } else {
+      // heartbeat
+      var requestBody = {
+        'timestamp': req.body.timestamp,
+        'reader': reader,
+        'antenna1': req.body.data.radio_control.antennas['1'],
+        'antenna2': req.body.data.radio_control.antennas['2'],
+        'antenna3': req.body.data.radio_control.antennas['3'],
+        'antenna4': req.body.data.radio_control.antennas['4']
+      }
+    }
+
+    console.log('requestBody',JSON.stringify(requestBody));
+
+
     if (reader) {
       if (Array.isArray(req.body)) {
         // If req.body is an array, add 'Reader' to each element
