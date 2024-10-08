7638b2d4b3fa30b106bb085be82fe427bc5f6b85
MotasTech <151187784+MotasTech@users.noreply.github.com>
2024-10-08 17:35:59 -0400

    Add files via upload
    Updated endpoint url parameter

--- pages/api/logRFID.js
+++ pages/api/logRFID.js
@@ -5,15 +5,22 @@ const GOOGLE_RFID_ENDPOINT = process.env.NEXT_PUBLIC_GOOGLE_RFID_ENDPOINT;
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    console.log('logRFID body',JSON.stringify(req.body))
+
+    // Get the query parameter `?reader=` from the URL
+    const { reader } = req.query;
+
+    // If the reader parameter exists, add it to the payload
+    if (reader) {
+      req.body.Reader = reader; // Add Reader to the payload
+    }
 
     axios.post(GOOGLE_RFID_ENDPOINT, req.body)
     .then((response) => {
-      console.log('Google Response',response.data);
+      console.log('Google Response',response.data,'body',JSON.stringify(req.body));
       res.status(response.status).json(response.data);
     })
     .catch((error) => {
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.status(500).json({ error: 'Internal Server Error','body': JSON.stringify(req.body) });
     });
 
   } else {
