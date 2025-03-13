d368bcabe238e47d3272c412c78397f7aad965db
chrisyoungr3 <95236766+chrisyoungr3@users.noreply.github.com>
2025-03-13 09:29:22 -0400

    fixed test endpoint to send back success

--- pages/api/testEndpoint.js
+++ pages/api/testEndpoint.js
@@ -2,7 +2,9 @@ import axios from 'axios'; // Import Axios
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    console.log('testEndpoint',JSON.stringify(req.body))
+    console.log('testEndpoint',JSON.stringify(req.body));
+    
+    res.status(200).json({ success: true });
 
   } else {
     // Return an error for other HTTP methods
