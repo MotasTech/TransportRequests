bd8699502a1890f7fab9ce368e701a6d777b6503
CHRIS YOUNG <christopher.young.r@gmail.com>
2025-03-21 11:15:17 -0400

    updated testEndpoint conflict

--- pages/api/testEndpoint.js
+++ pages/api/testEndpoint.js
@@ -1,9 +1,6 @@
 import axios from 'axios'; // Import Axios
 
-<<<<<<< HEAD
 // this just serves as a test endpoint I can see so I can see the payload in logs in vercel to
-=======
->>>>>>> origin/main
 export default function handler(req, res) {
   if (req.method === 'POST') {
     console.log('testEndpoint',JSON.stringify(req.body));
